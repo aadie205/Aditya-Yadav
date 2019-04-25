b6c0931d00898e09b6b8b2f3d018daa062d6f4c3
aadie205 <yadavaditya205@gmail.com>
2019-04-25 21:01:07 +0530

    Update credit.py

--- credit.py
+++ credit.py
@@ -3,9 +3,7 @@ print("Content-type:text/html")
 print("")
 import cgi,cgitb,pymysql
 cgitb.enable()
-
 form=cgi.FieldStorage()
-
 db=pymysql.connect(host='localhost',user='root1',password='root1',database='creditmng')
 cursor=db.cursor()
 query="select * from user"
@@ -37,25 +35,22 @@ print("""
 				""")
 
 for x in result:
-	print('<tr>')
-	print("<td>",x[1],"</td>")
-	print('<td>',x[6],'</td>')
-	print('<td>',x[2],'</td>')
-	print('<td>',x[3],'</td>')
-	print('<td>',x[4],'</td>')
-	print('<td>',x[5],'</td>')
-	print("<td><a href='new.py?uaccno1="+x[6]+"'><input type='submit' name='uaccno1' value='"+x[6]+"'></a></td>")
-	print('</tr>')
-
+print('<tr>')
+print("<td>",x[1],"</td>")
+print('<td>',x[6],'</td>')
+print('<td>',x[2],'</td>')
+print('<td>',x[3],'</td>')
+print('<td>',x[4],'</td>')
+print('<td>',x[5],'</td>')
+print("<td><a href='new.py?uaccno1="+x[6]+"'><input type='submit' name='uaccno1' value='"+x[6]+"'></a></td>")
+print('</tr>')
 print("""
 			</table>
 			</form>
 			</center>
 		</body>
 	</html>
-
-
-	""")
+""")
 
 submit=form.getvalue('submit')
 uaccno1=form.getvalue('uaccno1')
