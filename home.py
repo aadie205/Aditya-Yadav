a2cc0a5744b4969822cc94871bbf5718edb06a49
aadie205 <yadavaditya205@gmail.com>
2019-04-25 21:24:04 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,26 +4,27 @@ print("")
 print("""
 <html>
 <head>
-	<title>home page</title>
-	<link rel="icon" href="img/CM.png" />
-	<style type="text/css">
-	div{	color: black;
-		text-align: center;
-		background-color: AA8239;
-		height: 70px;
-		outline: 1px solid black;
-	}
-	</style>
+<title>home page</title>
+<link rel="icon" href="img/CM.png" />
+<style type="text/css">
+div{	
+color: blue;
+text-align: center;
+background-color: AA8239;
+height: 70px;
+outline: 1px solid black;
+}
+</style>
 </head>
 <body bgcolor="AA4139">
-	<center>
-		<img src="img/CM.png" height="50" width="50"><br><br>
-		This is Credit Management System
-	</center>
-	<br><br>
-	<div id="jan" >
-		<a href="credit.py">Credit Transfer</a>
-	</div>
+<center>
+<img src="img/CM.png" height="50" width="50"><br><br>
+This is Credit Management System
+</center>
+<br><br>
+<div id="jan" >
+<a href="credit.py">Credit Transfer</a>
+</div>
 </body>
 </html>
 """)
